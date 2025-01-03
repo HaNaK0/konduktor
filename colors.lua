3ce4bcf6cb15a4fb09931058a66e2a561b0536d7
HaNaK0 <hampus@hampushuledal.com>
2025-01-03 13:58:15 +0100

    add colors
    add file to store colors in

---
+++ colors.lua
@@ -0,0 +1,14 @@
+local t
+
+t.colors = {
+    RED = {1, 0, 0},
+    GREEN = {0, 1, 0},
+    BLUE = {0, 0, 1},
+}
+
+t.my_colors = {
+    table_color = {0.561, 0.278, 0.016},
+    ticket_color = {0.02, 1, 0.573}
+}
+
+return t
