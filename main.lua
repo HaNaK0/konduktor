3dd2e22f08e455d8e4c3535532b132221c85b94a
HaNaK0 <hampus@hampushuledal.com>
2025-01-03 13:04:22 +0100

    add table

--- main.lua
+++ main.lua
@@ -1,3 +1,21 @@
+local util = require("util")
+
+function love.load()
+    
+end
+
 function love.draw()
-    love.graphics.print('Hello World!', 400, 300)
+    love.graphics.setBackgroundColor(0.188, 0.757, 1, 1)
+
+    -- Draw table
+    love.graphics.setColor(0.561, 0.278, 0.016)
+
+    local x , y = util.screen_space(0.05, 0.5)
+    local width, height = util.screen_space(0.9, 0.45)
+
+    love.graphics.rectangle("fill", x, y, width, height )
+end
+
+function love.update(dt)
+    
 end
