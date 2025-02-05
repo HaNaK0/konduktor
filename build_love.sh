b4b9e8e360e3e74de382d81075d6be6efa18e7c7
HaNa <hampus@hampushuledal.com>
2025-02-05 17:26:03 +0100

    change to filesync
    add -FS to the zip command to speed up build
    remove deleting the old archive

--- build_love.sh
+++ build_love.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-if [ -f game.love ]; then
-	rm game.love
-fi
-zip -r -i "*.lua" "assets/*" @ game.love . && \
+#if [ -f game.love ]; then
+#	rm game.love
+#fi
+zip -FS -r -i "*.lua" "assets/*" @ game.love . && \
 	cp -v game.love ~/storage/shared/Documents/konduktör/game.love 
