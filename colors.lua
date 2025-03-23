c353fc07ad9f345c4ba776c8aa7b51c4daee0c29
HaNaK0 <hampus@hampushuledal.com>
2025-03-23 09:00:24 +0100

    update colors module to be global

--- colors.lua
+++ colors.lua
@@ -1,8 +1,8 @@
-local t = {}
+Colors = {}
 ---@alias Color [number, number, number] | [number, number, number, number]
 
 ---@enum color
-t.colors = {
+Colors.colors = {
 	RED = { 1, 0, 0 },
 	GREEN = { 0, 1, 0 },
 	BLUE = { 0, 0, 1 },
@@ -11,9 +11,9 @@ t.colors = {
 }
 
 ---@enum my_colors
-t.my_colors = {
+Colors.my_colors = {
 	table_color = { 0.561, 0.278, 0.016 },
 	ticket_color = { 0.02, 1, 0.573 },
 }
 
-return t
+return Colors
